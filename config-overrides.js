8ecbb6d916585ec7ab8bf8939fbcbab7b271a1d6
NeboMilic <nebo.milic@gmail.com>
2020-02-27 16:30:35 +0100

    Override antd variables via theme.less

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,17 @@
-const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const path = require('path')
+// eslint-disable-next-line no-undef
 module.exports = override(
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: true
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
   }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { "@primary-color": "#1DA57A" }
+    modifyVars: {
+      hack: `true; @import "${path.resolve(__dirname, './src/theme.less')}";`,
+    },
   })
-);
+)
